77db9f31bca7754cf004a0a1181f2d24df7c41cf
Imanol <imanolamado@hotmail.com>
2024-12-26 20:00:43 +0100

    commit aut

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $request->validate([        
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            
+        ]);
+              
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);        
+        
+        $user->save();       
+        return response()->json($user, 201);   
+    }
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ObraController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 
 /*
@@ -24,13 +25,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::controller(ObraController::class)->group(function(){
      
+    // Rutas "get"
     Route::get('/obras/{categoria}', 'categoria')->name('obras.categoria'); 
     Route::get('/obras/todos/{id}', 'show')->name('obras.show');
 
-    Route::middleware('auth:sanctum')->post('/obras/', 'store')->name('obras.store');
+    // Rutas "CRUD" para usuarios autenticados
+    Route::middleware('auth:sanctum')->post('/obras', 'store')->name('obras.store');
     Route::middleware('auth:sanctum')->put('/obras/{id}', 'update')->name('update.obras');
     Route::middleware('auth:sanctum')->delete('/obras/{id}', 'destroy')->name('delete.obas');
 });
 
 
+// Registrar nuevos usuarios
+Route::post('register', [UserController::class, 'register']);
+
+// Login usuarios
 Route::post('login', [AuthController::class, 'login']);
+
+// Logout usuarios
+Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
